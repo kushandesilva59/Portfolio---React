135ccca7448fa41801702f182863c4ecb90bb8fc
kushandesilva59 <kushandesilva801@gmail.com>
2023-11-11 20:54:14 +0530

    add animations to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import css from './Header.module.scss'
-import {BiPhoneCall} from 'react-icons/bi'
-
+import { BiPhoneCall } from 'react-icons/bi'
+import { motion } from 'framer-motion'
+import {headerVariants} from '../../utils/motion'
 
 const Header = () => {
     return (
-        <div className={`paddings ${css.wrapper}`}>
+        <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={headerVariants}
+            viewport={{once: false, amount: 0.25}}
+            className={`paddings ${css.wrapper}`}>
+            
             <div className={`flexCenter innerWidth ${css.container}`}>
                 <div className={css.name}>
                     Kushan
@@ -18,11 +25,11 @@ const Header = () => {
                     <li><a href="">Test</a></li>
                     <li className={`flexCenter ${css.phone}`}>
                         <p>+94 778 643 245 </p>
-                        <BiPhoneCall size={"40px"}/>
-                        </li>
+                        <BiPhoneCall size={"40px"} />
+                    </li>
                 </ul>
             </div>
-        </div>
+        </motion.div>
     )
 }
 
