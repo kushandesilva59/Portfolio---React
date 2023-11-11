df84395c852cce86e4e0d39a6b227672a094ae93
kushandesilva59 <kushandesilva801@gmail.com>
2023-11-11 22:08:24 +0530

    use shadow effect to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import css from './Header.module.scss'
-import { BiPhoneCall } from 'react-icons/bi'
+import { BiMenuAltRight, BiPhoneCall } from 'react-icons/bi'
 import { motion } from 'framer-motion'
-import {headerVariants} from '../../utils/motion'
+import {getMenuStyles, headerVariants} from '../../utils/motion'
+import useHeaderShadow from '../../hooks/useHeaderShadow'
+
 
 const Header = () => {
+
+    const [menuOpened, setMenuOpened] = useState(false)
+    const headerShadow = useHeaderShadow()
+
     return (
         <motion.div
             initial="hidden"
             whileInView="show"
             variants={headerVariants}
             viewport={{once: false, amount: 0.25}}
-            className={`paddings ${css.wrapper}`}>
+            className={`paddings ${css.wrapper}`
+            }
+            style={{boxShadow: headerShadow}}
+            >
             
             <div className={`flexCenter innerWidth ${css.container}`}>
                 <div className={css.name}>
                     Kushan
                 </div>
 
-                <ul className={`flexCenter ${css.menu}`}>
+                <ul style={getMenuStyles(menuOpened)}
+                className={`flexCenter ${css.menu}`}>
                     <li><a href="">Services</a></li>
                     <li><a href="">Experience</a></li>
                     <li><a href="">Portfolio</a></li>
@@ -28,6 +38,10 @@ const Header = () => {
                         <BiPhoneCall size={"40px"} />
                     </li>
                 </ul>
+
+                <div className={css.menuIcon} onClick={()=>setMenuOpened((prev)=>!prev)}>
+                    <BiMenuAltRight size={30}/>
+                </div>
             </div>
         </motion.div>
     )
